fe3016a03c1a1d36793ea1cf4b11a8858f06cbdb
Khishgee <n.khishigjargal99@gmail.com>
2023-12-24 15:34:25 +0800

    finish project fixed small bugs in index.html and styles.css also

--- app.js
+++ app.js
@@ -11,23 +11,6 @@ const btnColor = number => {
     else return 'gray';
 }
 
-// decrBtn.addEventListener('click', e => {
-//     count--;
-//     num.textContent = count;
-//     num.style.color = btnColor(count);
-// });
-
-// incBtn.addEventListener('click', e => {
-//     count++;
-//     num.textContent = count;
-//     num.style.color = btnColor(count);
-// });
-// resBtn.addEventListener('click', e => {
-//     count = 0;
-//     num.textContent = count;
-//     num.style.color = btnColor(count);
-// });
-
 btnDiv.addEventListener('click', e=>{
     if(e.target.matches("#increase, #increase *")) count++;
     else if(e.target.matches("#decrease, #decrease *")) count--; 
