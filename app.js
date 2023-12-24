78c5b8b1d606142038f56e0ca5d7cfeb74b89791
Khishgee <n.khishigjargal99@gmail.com>
2023-12-24 15:33:54 +0800

    finish project fixed small bugs in index.html and styles.css also

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const num = document.getElementById('num');
 const decrBtn = document.getElementById('decrease');
 const incBtn = document.getElementById('increase');
 const resBtn = document.getElementById('reset');
-
+const btnDiv = document.getElementById('button-div');
 
 const btnColor = number => { 
     if(number > 0) return 'green';
@@ -11,22 +11,30 @@ const btnColor = number => {
     else return 'gray';
 }
 
-decrBtn.addEventListener('click', e => {
-    count--;
-    num.textContent = count;
-    num.style.color = btnColor(count);
-});
+// decrBtn.addEventListener('click', e => {
+//     count--;
+//     num.textContent = count;
+//     num.style.color = btnColor(count);
+// });
 
-incBtn.addEventListener('click', e => {
-    count++;
+// incBtn.addEventListener('click', e => {
+//     count++;
+//     num.textContent = count;
+//     num.style.color = btnColor(count);
+// });
+// resBtn.addEventListener('click', e => {
+//     count = 0;
+//     num.textContent = count;
+//     num.style.color = btnColor(count);
+// });
+
+btnDiv.addEventListener('click', e=>{
+    if(e.target.matches("#increase, #increase *")) count++;
+    else if(e.target.matches("#decrease, #decrease *")) count--; 
+    else if(e.target.matches("#reset, #reset *")) count = 0;
     num.textContent = count;
     num.style.color = btnColor(count);
-});
-resBtn.addEventListener('click', e => {
-    count = 0;
-    num.textContent = count;
-    num.style.color = btnColor(count);
-});
+})
 
 
 
